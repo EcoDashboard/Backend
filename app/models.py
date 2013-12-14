9f3ac69f01deb9613036b1eb9aa0afc905cb24ff
Leon <leon@leon-Ubuntu.(none)>
2013-12-14 11:42:35 +1030

    new

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
-from app import db
-from sqlalchemy import Column, Integer, String
-
+from sqlalchemy import Table, Column, Integer, \
+        String, MetaData, join, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import column_property
+from app import db 
 
 class user_data(db.Model):
     __tablename__ = 'user_data'
@@ -15,12 +17,12 @@ class user_data(db.Model):
 
 class indicator_def(db.Model):
     __tablename__ = 'indicator_def'
-    ind_id = db.Column(db.String(40), primary_key = True)
+    ind_id = db.Column(db.Integer, primary_key = True)
     ind_name = db.Column(db.String(100), unique=False)
     designation = db.Column(db.String(100), unique=False)
     unit = db.Column(db.String(100), unique=False)
     description = db.Column(db.String(200), unique=False)
-    category_id = db.Column(db.String(40), db.ForeignKey('category_def.cat_id'))
+    category_id = db.Column(db.Integer, db.ForeignKey('category_def.cat_id'))
     weight = db.Column(db.Integer, unique=False)
     upper_value = db.Column(db.Integer, unique=False)
     lower_value = db.Column(db.Integer, unique=False)
@@ -29,22 +31,9 @@ class indicator_def(db.Model):
     def __repr__(self):
         return '<indicator_def %r>' % (self.ind_id ) 
 
-    def returnString(self):
-    	return { 'ind_id' : self.ind_id	,
-		'ind_name' : self.ind_name,
-		'designation' : self.designation,
-		'unit' : self.unit,
-		'description' : self.description,
-		'category_id' : self.category_id,
-		'weight' : int(self.weight),
-		'upper_value' : int(self.upper_value),
-		'lower_value' : int(self.lower_value),
-		'target_value' : int(self.target_value)
-		}
-
 class category_def(db.Model):
     __tablename__ = 'category_def'
-    cat_id = db.Column(db.String(40), primary_key = True)
+    cat_id = db.Column(db.Integer, primary_key = True)
     cat_name = db.Column(db.String(100), unique=False)
     cat_id_parent = db.Column(db.String(10), unique=False)
     weight = db.Column(db.Integer, unique=False)
@@ -52,3 +41,36 @@ class category_def(db.Model):
     def __repr__(self):
         return '<category_def %r>' % (self.cat_id)
 
+metadata = MetaData()
+
+indicator_def_join = Table('indicator_def', metadata,
+	Column('ind_id', Integer, primary_key=True),
+        Column('ind_name', String),
+	Column('category_id', Integer, ForeignKey('category_def.cat_id'))
+        )
+category_def_join = Table( 'category_def', metadata,
+	Column('cat_id', Integer, primary_key=True),
+	Column('cat_name', String)	            
+	)
+
+showIndcator_join = indicator_def_join.join(category_def_join)
+Base = declarative_base()
+
+class showIndicators(Base):
+    __table__ = showIndcator_join
+    ind_id = indicator_def_join.c.ind_id
+    cat_name = category_def_join.c.cat_name
+    def returnString(self):
+    	return { 'ind_id' : self.ind_id,
+		'cat_name' : self.cat_name
+	     #'ind_name' : showIndcator_join.ind_name,
+	     #'designation' : indicator_def.designation,
+	     #'unit' : indicator_def.unit,
+	     #'description' : indicator_def.description,
+	     #'category_id' : showIndcator_join.category_id,
+	     #'cat_name' : showIndcator_join.cat_name,
+	     #'weight' : int(indicator_def.weight),
+	     #'upper_value' : int(indicator_def.upper_value),
+	     #'lower_value' : int(indicator_def.lower_value),
+	     #'target_value' : int(indicator_def.target_value)
+		}
