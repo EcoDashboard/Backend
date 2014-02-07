b3553e88b6919141aab8e122ad64142e74045953
Sean <sean@sean-Lenovo-V360.(none)>
2014-02-07 16:26:22 +1030

    lalalala

--- app/views.py
+++ app/views.py
@@ -1,29 +1,37 @@
 from flask import render_template
 from app import app
 import json
-from flask import request, jsonify
-from app import models, db
+from flask import request, jsonify, session, escape
+from app import models, db 
 import hashlib
 
 users={'naveen':'password','hanwei':123456}
+
 @app.route('/')
 @app.route('/index')
 def index():
-	return render_template("index.html")
+    if 'email' in session:
+        return '{email:%s, login=true}' % escape(session['email'])
+    return render_template("index.html")
 
 @app.route('/login', methods=['POST'])
-
 def login():
-	
-	user=request.form['user_name']
+	email=request.form['email']
 	password=request.form['password']
-	if user in users:
-		if password ==users.get(user):
-			return json.dumps({"naveen":"wazza","hanwei":"hello"})
-		else:
-			return render_template("index.html",title ='LOGIN PAGE')
+	
+	userInDB = models.user_data.query.filter(models.user_data.email == email).filter(models.user_data.password == hashlib.sha256(password).hexdigest()).all()
+	
+	rs = False
+	
+	if len(userInDB) == 1:
+		rs = True
+		session['email'] = request.form['email']
 	else:
-		return "Username or password wrong"
+		rs = False
+		
+	return str(rs)
+        '''return redirect(url_for('index'))
+    return render_template("index.html")'''
 
 @app.route('/register', methods=['POST'])
 
@@ -50,15 +58,7 @@ def saveDetails():
 def indicatorList():
 	#indicators = models.indicator_def.query.get('1')
 	#return  jsonify(indicators.returnString())
-	Indicators = db.session.query(models.showIndicators)
-	Air_ind = Indicators.filter(models.showIndicators.cat_id == 1)
-	Water_ind = Indicators.filter(models.showIndicators.cat_id == 2)
-	Land_ind = Indicators.filter(models.showIndicators.cat_id == 3)
-	Energy_ind = Indicators.filter(models.showIndicators.cat_id == 4)
-	Bio_Diversity_ind = Indicators.filter(models.showIndicators.cat_id == 5)
-	return  jsonify( Air = [i.returnString() for i in Air_ind],
-			 Water = [i.returnString() for i in Water_ind],
-			 Land = [i.returnString() for i in Land_ind],
-			 Energy = [i.returnString() for i in Energy_ind],
-			 Bio_Diversity = [i.returnString() for i in Bio_Diversity_ind])
-#0000000000000000000
+	indicators = models.indicator_def.query.all()
+	return  jsonify(json_list=[i.returnString() for i in indicators])
+		
+#lalalallala
