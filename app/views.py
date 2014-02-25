77f8af166dcbc31aec3c1be0fdacaac70fae30b1
Daniel Alan Miller <dalan.miller@gmail.com>
2014-02-25 23:58:00 +1030

    Adding Flask-Cors as a requirement and adding first CORS test to registration view

--- app/views.py
+++ app/views.py
@@ -2,7 +2,9 @@ from flask import render_template
 from app import app
 import json
 from flask import request, jsonify, session, escape, Response
-from app import models, db 
+from app import models, db
+from flask.ext.cors import cross_origin
+
 import hashlib
 
 users={'naveen':'password','hanwei':123456}
@@ -14,7 +16,7 @@ def default():
 @app.route('/login_test.html', methods=['GET'])
 def index():
     return render_template("index.html")
-	
+
 @app.route('/login', methods=['GET'])
 def loginGet():
     if 'email' in session:
@@ -25,17 +27,17 @@ def loginGet():
 def loginPost():
 	email=request.form['email']
 	password=request.form['password']
-	
+
 	userInDB = models.user_data.query.filter(models.user_data.email == email).filter(models.user_data.password == hashlib.sha256(password).hexdigest()).all()
-	
+
 	rs = False
-	
+
 	if len(userInDB) == 1:
 		rs = True
 		session['email'] = request.form['email']
 	else:
 		rs = False
-		
+
 	return str(rs)
         '''return redirect(url_for('index'))
     return render_template("index.html")'''
@@ -51,20 +53,21 @@ def registerTest():
 	return render_template("register_test.html")
 
 @app.route('/register', methods=['POST'])
+@cross_origin()
 def register():
 	email=request.form['email'].strip()
 	if not email:
 		return 'email is empty'
-		
+
 	user_name=request.form['user_name']
 	password=request.form['password']
 	hash = hashlib.sha256(password).hexdigest()
 	city_id_admin = request.form['city_id_admin']
 	email=request.form['email']
 
-	u = models.user_data(user_name=user_name, password=hash, city_id_admin=city_id_admin, email=email)	
+	u = models.user_data(user_name=user_name, password=hash, city_id_admin=city_id_admin, email=email)
 	db.session.add(u)
-	db.session.commit()	
+	db.session.commit()
 	db.session.close()
 
 	return 'true'
@@ -78,7 +81,7 @@ def getUserData():
 		return Response(json.dumps(list[0]), mimetype='application/json')
 	else:
 		return 'false'
-	
+
 
 @app.route('/getCityList', methods=['GET'])
 def getCityList():
@@ -91,8 +94,8 @@ def indicatorList():
 	#indicators = models.indicator_def.query.get('1')
 	#return  jsonify(indicators.returnString())
 	Indicators = db.session.query(models.showIndicators)
-	
-	
+
+
 	Air_ind = Indicators.filter(models.showIndicators.cat_id == 1)
 	Water_ind = Indicators.filter(models.showIndicators.cat_id == 2)
 	Land_ind = Indicators.filter(models.showIndicators.cat_id == 3)
