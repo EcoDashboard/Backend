cc64a0893ea925c20a219cc4b1658aaefa06a811
Daniel Alan Miller <dalan.miller@gmail.com>
2014-02-26 20:47:54 +1030

    Fixing path

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config.from_pyfile('config.py')
+app.config.from_pyfile('../config.py')
 db = SQLAlchemy(app)
 
 from app import views, models
--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 CSRF_ENABLED = False
 SECRET_KEY = 'you-will-never-guess'
-# DEBUG = True
+DEBUG = True
 
 SQLALCHEMY_DATABASE_URI = "postgres://dduygeysuvusfe:OW-caHT4Gt3LT3EPEFuOVyCh7d@ec2-54-235-117-198.compute-1.amazonaws.com:5432/deng2iv15mnmrd"
 
