8d437b4e2e71e0b4751f6bb5b72de05ae991b8ed
Daniel Alan Miller <dalan.miller@gmail.com>
2014-03-04 15:41:55 +1030

    All the origins try #2

--- app/views.py
+++ app/views.py
@@ -7,11 +7,56 @@ from flask.ext.httpauth import HTTPBasicAuth
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from sqlalchemy import func
 from urlparse import urlparse, urlunparse
+from datetime import timedelta
+from functools import update_wrapper
+
 import hashlib
 import json
 import re
 import urllib
 
+auth = HTTPBasicAuth()
+
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
 
 def generate_auth_token(expiration = 600):
     s = Serializer(app.config['SECRET_KEY'], expires_in = expiration)
@@ -26,7 +71,7 @@ def verify_auth_token(token):
     user = models.user_data.query.filter(models.user_data.user_id == data['id']).all()
     return user[0]
 
-@auth.verify_password
+
 def verify_password(username_or_token, password):
     # first try to authenticate by token
     user = verify_auth_token(username_or_token)
@@ -59,7 +104,7 @@ def index():
     return render_template("index.html")
 
 @app.route('/login', methods=['POST'])
-@cross_origin(origins="*")
+@crossdomain(origin='*')
 def loginPost():
 
     email=urllib.unquote(request.form['email'])
@@ -115,7 +160,7 @@ def checkCityIdExists(city_id):
 
 
 @app.route('/register', methods=['POST'])
-@cross_origin(origins="*")
+@crossdomain(origin='*')
 def register():
 
     # print request.form
@@ -203,7 +248,7 @@ def register():
     return 'true'
 
 @app.route('/getUserData')
-@cross_origin(origins="*")
+@crossdomain(origin='*')
 # @auth.login_required
 def getUserData():
     if 'email' in session:
@@ -216,7 +261,7 @@ def getUserData():
 
 
 @app.route('/getCityProfile')
-@cross_origin(origins="*")
+@crossdomain(origin='*')
 def getCityProfile():
     city_id = request.values.get("city")
     if city_id:
