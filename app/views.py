bc9429c66b73672c775c511576b6fc213fa85215
Daniel Alan Miller <dalan.miller@gmail.com>
2014-03-04 12:55:41 +1030

    Ch ch ch changes

--- app/views.py
+++ app/views.py
@@ -1,22 +1,24 @@
 from app import app
 from app import models, db
 from flask import render_template
-from flask import request, jsonify, session, escape, Response, g, make_response
+from flask import request, jsonify, session, escape, Response, g, make_response, redirect
 from flask.ext.cors import cross_origin
 from flask.ext.httpauth import HTTPBasicAuth
-from sqlalchemy import func
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from sqlalchemy import func
+from urlparse import urlparse, urlunparse
 import hashlib
 import json
-import urllib
 import re
+import urllib
+
 
 auth = HTTPBasicAuth()
 
 
 def generate_auth_token(expiration = 600):
-	s = Serializer(app.config['SECRET_KEY'], expires_in = expiration)
-	return s.dumps({ 'id': g.user.user_id })
+    s = Serializer(app.config['SECRET_KEY'], expires_in = expiration)
+    return s.dumps({ 'id': g.user.user_id })
 
 def verify_auth_token(token):
     s = Serializer(app.config['SECRET_KEY'])
@@ -37,8 +39,8 @@ def verify_password(username_or_token, password):
         user = models.user_data.query.filter(models.user_data.user_name == username_or_token).first()
 
         user2 = models.user_data.query.filter(\
-        		models.user_data.password == hashlib.sha256(password).hexdigest()\
-        		).all()
+                models.user_data.password == hashlib.sha256(password).hexdigest()\
+                ).all()
 
         if not user or len(user2) != 1:
             return False
@@ -53,175 +55,191 @@ def get_auth_token():
 
 @app.route('/')
 def default():
-	return ''
+    return ''
 
 @app.route('/login_test.html', methods=['GET'])
 def index():
     return render_template("index.html")
 
-
-
 @app.route('/login', methods=['POST'])
 def loginPost():
-	print 'HELLO2'
-	print request.form
-	email=urllib.unquote(request.form['email'])
-	password=request.form['password']
 
-	user = models.user_data.query.filter(models.user_data.email == email)\
-		.filter(models.user_data.password == hashlib.sha256(password).hexdigest())\
-		.first()
+    email=urllib.unquote(request.form['email'])
+    password=request.form['password']
 
-	print user
 
-	if user:
-		g.user = user
-		token = generate_auth_token()
-		return jsonify({'token':token})
-	else:
-		return make_response("",503)
+    user = models.user_data.query.filter(models.user_data.email == email)\
+        .filter(models.user_data.password == hashlib.sha256(password).hexdigest())\
+        .first()
+
+    #Stripping path onwards from url for redirect
+    u = urlparse(request.url)[:2] + ('/dashboard.html#','', '', '')
+
+    if user:
+        g.user = user
+        token = generate_auth_token()
+        jsonify({'token':token})
+        return redirect(u)
+    else:
+        return make_response("Cannot find user",401)
 
 
 @app.route('/logout', methods=['GET'])
 def logout():
-	if 'email' in session:
-		session.pop('email', None)
-		return 'true'
+    if 'email' in session:
+        session.pop('email', None)
+        return 'true'
 
 @app.route('/register_test.html')
 def registerTest():
-	return render_template("register_test.html")
+    return render_template("register_test.html")
 
 
 @app.route('/checkCityIdExists')
 def checkCityIdExists():
-	city_id = request.values.get("city_id");
-	return str(checkCityIdExists(city_id))
-		
+    city_id = request.values.get("city_id");
+    return str(checkCityIdExists(city_id))
+
 
 def checkCityIdExists(city_id):
-	if not city_id:
-		return True
-	elif models.city_profile_data.query.filter(models.city_profile_data.city_id == city_id).first():
-		return True
-	else:
-		return False
-	
+    if not city_id:
+        return True
+    elif models.city_profile_data.query.filter(models.city_profile_data.city_id == city_id).first():
+        return True
+    else:
+        return False
+
 
 @app.route('/register', methods=['POST'])
 @cross_origin()
 def register():
 
-	# user
-	email = urllib.unquote(request.form['email'].strip()).lower()
-	password=request.form['password']
-	hash = hashlib.sha256(password).hexdigest()
-	contact_firstname = request.form['contact_firstname'].strip()
-	contact_lastname = request.form['contact_lastname'].strip()
-		
-	# city
-	city_id = request.form['city_abbre'].strip().upper()
-	city_name = request.form['city_name'].strip()
-	council_name = request.form['council_name'].strip()
-	state = request.form['state'].strip()
-	country = request.form['country'].strip()
-	postcode = request.form['postcode'].strip()
-	area = request.form['area'].strip()
-	population = request.form['population'].strip()
-	contact_email = request.form['contact_email'].strip()
-	contact_number = request.form['contact_number'].strip()
-	council_address = request.form['council_address'].strip()
+    # print request.form
 
-	# check
-	errors=[]
-	if not email:
-		errors.append('Email is empty.')
-	elif  models.user_data.query.filter(models.user_data.email == email).first():
-		errors.append('Email has already been used.')
-	
-	if not city_id:
-		errors.append('City abbreviation is empty.')
-	elif checkCityIdExists(city_id):
-		errors.append('City Alias has already been used.')
+    needed_keys = ['email','password','contact_firstname','contact_lastname', \
+            'city_abbre','city_name','council_name', 'state', 'country',\
+            'postcode', 'area', 'population', 'contact_email',\
+            'contact_number', 'council_address']
 
-	if not re.match("^[0-9]*[.]?[0-9]+$", area):
-		errors.append('Area format is incorrect.')
+    print set(needed_keys) - set(request.form.keys())
 
-	# invalid input
-	if errors:
-		return Response(json.dumps(errors), mimetype='application/json')
+    if len(set(needed_keys) - set(request.form.keys())) != 0:
+        return make_response("Missing form fields",503)
 
-	#insert new city
-	city = models.city_profile_data(
-		city_id = city_id,
-		city_name = city_name,
-		country = country,
-		state = state,
-		post_code =  postcode,
-		population =  population,
-		area = area,
-		council_name = council_name ,
-		contact_email = contact_email,
-		contact_number = contact_number,
-		council_address = council_address)
-	db.session.add(city)
-	db.session.commit()
+    # user
+    email = urllib.unquote(request.form['email'].strip()).lower()
+    password=request.form['password']
+    hash = hashlib.sha256(password).hexdigest()
+    contact_firstname = request.form['contact_firstname'].strip()
+    contact_lastname = request.form['contact_lastname'].strip()
 
-	#insert new user, assign city_admin
-	u = models.user_data(
-		first_name=contact_firstname, 
-		last_name = contact_lastname, 
-		password=hash, 
-		city_id_admin=city_id, 
-		email=email)
-	db.session.add(u)
-	db.session.commit()
-	db.session.close()
+    print 'User fine'
 
-	return 'true'
+    # city
+    city_id = request.form['city_abbre'].strip().upper()
+    city_name = request.form['city_name'].strip()
+    council_name = request.form['council_name'].strip()
+    state = request.form['state'].strip()
+    country = request.form['country'].strip()
+    postcode = request.form['postcode'].strip()
+    area = request.form['area'].strip()
+    population = request.form['population'].strip()
+    contact_email = request.form['contact_email'].strip()
+    contact_number = request.form['contact_number'].strip()
+    council_address = request.form['council_address'].strip()
+
+    # check
+    errors=[]
+    if not email:
+        errors.append('Email is empty.')
+    elif  models.user_data.query.filter(models.user_data.email == email).first():
+        errors.append('Email has already been used.')
+
+    if not city_id:
+        errors.append('City abbreviation is empty.')
+    elif checkCityIdExists(city_id):
+        errors.append('City Alias has already been used.')
+
+    if not re.match("^[0-9]*[.]?[0-9]+$", area):
+        errors.append('Area format is incorrect.')
+
+    # invalid input
+    if errors:
+        return Response(json.dumps(errors), mimetype='application/json')
+
+    print "No errors!"
+
+    #insert new city
+    city = models.city_profile_data(
+        city_id = city_id,
+        city_name = city_name,
+        country = country,
+        state = state,
+        post_code =  postcode,
+        population =  population,
+        area = area,
+        council_name = council_name ,
+        contact_email = contact_email,
+        contact_number = contact_number,
+        council_address = council_address)
+    db.session.add(city)
+    db.session.commit()
+
+    #insert new user, assign city_admin
+    u = models.user_data(
+        first_name=contact_firstname,
+        last_name = contact_lastname,
+        password=hash,
+        city_id_admin=city_id,
+        email=email)
+    db.session.add(u)
+    db.session.commit()
+    db.session.close()
+
+    return 'true'
 
 @app.route('/getUserData')
 @auth.login_required
 def getUserData():
-	if 'email' in session:
-		email=session['email'].strip()
-		userData = models.user_data.query.filter(models.user_data.email == email).all()
-		list = [i.returnString() for i in userData]
-		return Response(json.dumps(list[0]), mimetype='application/json')
-	else:
-		return 'false'
+    if 'email' in session:
+        email=session['email'].strip()
+        userData = models.user_data.query.filter(models.user_data.email == email).all()
+        list = [i.returnString() for i in userData]
+        return Response(json.dumps(list[0]), mimetype='application/json')
+    else:
+        return 'false'
 
-	
+
 @app.route('/getCityProfile')
 def getCityProfile():
-	city_id = request.values.get("city")
-	if city_id:
-		city_id = city_id.strip()
-		profiles = db.session\
-			.query(models.showUsers)\
-			.filter(models.showUsers.city_id == city_id)\
-			.all()
-		list = [i.returnString() for i in profiles]
-		return Response(json.dumps(list[0]), mimetype='application/json')
-	else:
-		profiles = db.session.query(models.showUsers).all()
-		list = [i.returnString() for i in profiles]
-		return Response(json.dumps(list), mimetype='application/json')
+    city_id = request.values.get("city")
+    if city_id:
+        city_id = city_id.strip()
+        profiles = db.session\
+            .query(models.showUsers)\
+            .filter(models.showUsers.city_id == city_id)\
+            .all()
+        list = [i.returnString() for i in profiles]
+        return Response(json.dumps(list[0]), mimetype='application/json')
+    else:
+        profiles = db.session.query(models.showUsers).all()
+        list = [i.returnString() for i in profiles]
+        return Response(json.dumps(list), mimetype='application/json')
 
 @app.route('/indicatorList', methods=['GET'])
 def indicatorList():
-	#indicators = models.indicator_def.query.get('1')
-	#return  jsonify(indicators.returnString())
-	Indicators = db.session.query(models.showIndicators)
+    #indicators = models.indicator_def.query.get('1')
+    #return  jsonify(indicators.returnString())
+    Indicators = db.session.query(models.showIndicators)
 
 
-	Air_ind = Indicators.filter(models.showIndicators.cat_id == 1)
-	Water_ind = Indicators.filter(models.showIndicators.cat_id == 2)
-	Land_ind = Indicators.filter(models.showIndicators.cat_id == 3)
-	Energy_ind = Indicators.filter(models.showIndicators.cat_id == 4)
-	Bio_Diversity_ind = Indicators.filter(models.showIndicators.cat_id == 5)
-	return  jsonify( Air = [i.returnString() for i in Air_ind],
-			 Water = [i.returnString() for i in Water_ind],
-			 Land = [i.returnString() for i in Land_ind],
-			 Energy = [i.returnString() for i in Energy_ind],
-			 Bio_Diversity = [i.returnString() for i in Bio_Diversity_ind])
+    Air_ind = Indicators.filter(models.showIndicators.cat_id == 1)
+    Water_ind = Indicators.filter(models.showIndicators.cat_id == 2)
+    Land_ind = Indicators.filter(models.showIndicators.cat_id == 3)
+    Energy_ind = Indicators.filter(models.showIndicators.cat_id == 4)
+    Bio_Diversity_ind = Indicators.filter(models.showIndicators.cat_id == 5)
+    return  jsonify( Air = [i.returnString() for i in Air_ind],
+             Water = [i.returnString() for i in Water_ind],
+             Land = [i.returnString() for i in Land_ind],
+             Energy = [i.returnString() for i in Energy_ind],
+             Bio_Diversity = [i.returnString() for i in Bio_Diversity_ind])
